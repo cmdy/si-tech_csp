9b6c682897ac0c030d2fce1cbae425f550f5f84c
cmdy <zhang_lin66@foxmail.com>
2017-10-26 22:26:36 +0800

    Update startMonitor.sh

--- make/k8s_monitor/k8s_monitor/bin/startMonitor.sh
+++ make/k8s_monitor/k8s_monitor/bin/startMonitor.sh
@@ -9,7 +9,7 @@ AGT_CLASS="com.sitech.kubernetes.coll.CollAllRes"
 pro_count=`ps -ef | grep ${PRO_KEY} | grep -v grep | wc -l`
 if [ $pro_count -lt 1 ]
 then
-nohup java -D${PRO_KEY} -Djavax.management.builder.initial=mx4j.server.MX4JMBeanServerBuilder ${AGT_CLASS}  1>/dev/null 2>&1 &
+nohup java -D${PRO_KEY} -Djavax.management.builder.initial=mx4j.server.MX4JMBeanServerBuilder ${AGT_CLASS}  1>/dev/null 2>&1
 echo "k8sMonitor startup "
 else
 echo "there is already ${pro_count} process with key ${PRO_KEY} exists"
